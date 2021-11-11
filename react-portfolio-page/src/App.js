33cb157026657da88091189ecc93f5718749bc75
Khanh Truong <khanhtruong_1992@yahoo.com>
2021-11-10 18:19:27 -0800

    added components,pages,utils

--- react-portfolio-page/src/App.js
+++ react-portfolio-page/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import PortfolioPages from './pages/PortfolioPages';
+import { PortfolioProvider } from './utils/GlobalState';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<PortfolioProvider>
+			<PortfolioPages/>
+		</PortfolioProvider>
+	);
 }
-
 export default App;
